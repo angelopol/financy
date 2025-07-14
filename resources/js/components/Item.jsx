c5af934234bd3f4d45b76a6f1e6c1c9dc370a449
angelopol <angelopolgrossi18@gmail.com>
2025-07-14 18:04:32 -0400

    Added next claim date

--- resources/js/components/Item.jsx
+++ resources/js/components/Item.jsx
@@ -79,7 +79,10 @@ export default function Item({ item, Route, DestroyRoute, openRatesModal }) {
                 <h3 className="text-lg font-semibold">{item.description}</h3>
                 <p className="text-sm text-gray-500" onClick={() => openRatesModal(item.amount, item.currency ? item.currency : '$')}>{item.amount} {item.currency ? (getCurrencyLabel(item.currency)) : '$'}</p>
                 {item.term ? (
-                    <p className="text-sm text-gray-500">Claim cycle of {item.term} days</p>
+                    <>
+                        <p className="text-sm text-gray-500">Claim cycle of {item.term} days</p>
+                        <p className="text-sm text-gray-500">Next claim {dayjs(item.UpdatedTerm).add(item.term, 'day').fromNow()}</p>
+                    </>
                 ) : item.OneTimeTase ? (
                     <p className="text-sm text-gray-500">Parallel exchange tase of {item.OneTimeTase}</p>
                 ) : null}
