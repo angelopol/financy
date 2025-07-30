4e51dd5345c8928f3c1b82380fe7cd75f4ad0059
angelopol <angelopolgrossi18@gmail.com>
2025-07-30 12:12:54 -0400

    changed pagination

--- resources/js/components/Pagination.jsx
+++ resources/js/components/Pagination.jsx
@@ -1,26 +1,18 @@
 import { Link } from '@inertiajs/react';
 
 export default function Pagination({ links }) {
-    // Filtrar solo los links relevantes para mostrar máximo 3 páginas (anterior, actual, siguiente)
     let filteredLinks = [];
     const currentIndex = links.findIndex(link => link.active);
 
-    // Siempre mostrar el link de anterior si existe
-    if (links[0] && links[0].label.toLowerCase().includes('Previous')) {
-        filteredLinks.push(links[0]);
-    }
+    filteredLinks.push(links[0]);
 
-    // Mostrar hasta 3 páginas: anterior, actual, siguiente
     for (let i = currentIndex - 1; i <= currentIndex + 1; i++) {
         if (i > 0 && i < links.length - 1) {
             filteredLinks.push(links[i]);
         }
     }
 
-    // Siempre mostrar el link de siguiente si existe
-    if (links[links.length - 1] && links[links.length - 1].label.toLowerCase().includes('Next')) {
-        filteredLinks.push(links[links.length - 1]);
-    }
+    filteredLinks.push(links[links.length - 1]);
 
     return (
         <div className="flex flex-wrap justify-end mt-6">
