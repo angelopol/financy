70fd54fd5930dba92556ca33a71ee6921f151154
angelopol <angelopolgrossi18@gmail.com>
2025-07-30 13:13:57 -0400

    removing elipsis from pagination

--- resources/js/components/Pagination.jsx
+++ resources/js/components/Pagination.jsx
@@ -18,12 +18,6 @@ export default function Pagination({ links }) {
         );
     }
 
-    const createEllipsis = () => ({
-        url: null,
-        label: '...',
-        active: false,
-    });
-
     let filteredLinks = [];
     const currentIndex = links.findIndex(link => link.active);
     const lastPageIndex = links.length - 2;
@@ -36,40 +30,22 @@ export default function Pagination({ links }) {
 
     // 3. Intermediate 1
     const intermediate1Index = Math.floor((1 + currentIndex) / 2);
-    if (intermediate1Index > 2) {
-        filteredLinks.push(createEllipsis());
-    }
     if (intermediate1Index > 1 && intermediate1Index < currentIndex) {
         filteredLinks.push(links[intermediate1Index]);
     }
 
     // 4. Current page and its neighbours
     if (currentIndex > 1 && currentIndex < lastPageIndex) {
-        if (currentIndex - 1 > intermediate1Index && currentIndex -1 > 1) {
-             if (filteredLinks[filteredLinks.length - 1].label !== '...') {
-                filteredLinks.push(createEllipsis());
-             }
-        }
         filteredLinks.push(links[currentIndex]);
     }
 
     // 5. Intermediate 2
     const intermediate2Index = Math.floor((currentIndex + lastPageIndex) / 2);
      if (intermediate2Index > currentIndex && intermediate2Index < lastPageIndex) {
-        if (currentIndex + 1 < intermediate2Index) {
-            if (filteredLinks[filteredLinks.length - 1].label !== '...') {
-                filteredLinks.push(createEllipsis());
-            }
-        }
         filteredLinks.push(links[intermediate2Index]);
     }
 
     // 6. Last page
-    if (lastPageIndex > intermediate2Index) {
-        if (filteredLinks[filteredLinks.length - 1].label !== '...') {
-            filteredLinks.push(createEllipsis());
-        }
-    }
     filteredLinks.push(links[lastPageIndex]);
 
     // 7. Next button
